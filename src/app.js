e6728ede2d2ec6e419d845fa42df55d51cb10599
anetastankovska <anetastankovskaane@gmail.com>
2022-08-14 13:02:41 +0200

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const { Http2ServerRequest } = require('http2')
 const request = require('request')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
@@ -109,6 +110,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
